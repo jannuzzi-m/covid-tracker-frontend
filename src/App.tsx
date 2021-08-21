381f418caf5cebd65cd110dad3272e2e08392cb9
Marcelino Jannuzzi <jannuzzimarcelino1@gmail.com>
2021-08-20 22:22:17 -0300

    added routes and first components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import './App.css';
+import Menu from './components/Menu/Menu';
+import Map from './components/Map/Map';
+import Charts from './components/Charts/Charts';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { SVGMap } from "react-svg-map";
+import "react-svg-map/lib/index.css";
+import Brazil from "@svg-maps/brazil";
 
 const App = () => {
   return (
-    <div className="container">
-      <h1>Hello World</h1>
+    <div id="container">
+      <Router>
+        <div id="menu">
+          <Menu />
+        </div>
+        <main id="main">
+          <Switch>
+            <Route path="/map">
+              <Map />
+            </Route>
+            <Route path="/charts">
+              <Charts />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import './Menu.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Map from "../Map/Map";
+import Charts from "../Charts/Charts";
+const Menu = () => {
+    return (
+        <div className="menu-container">
+            <div id="logo-container">
+                <img id="logo" src="logo.png" alt="" />
+            </div>
+            <div id="menu-container">
+                <Link to='/map' className="menu-item">
+                    Map
+                </Link>
+                <Link to='/charts' className="menu-item">
+                    Charts
+                </Link>
+            </div>
+        </div>
+    )
+}
+export default Menu;
